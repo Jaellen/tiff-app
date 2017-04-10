93cb306c068daa55da06ede66b393ba4e6bc0e81
Jonathan Aellen <aellen.j@gmail.com>
2017-04-09 21:43:32 -0400

    Move FeaturedMovie's state up to TiffApp

--- app/components/Featured.jsx
+++ app/components/Featured.jsx
@@ -2,82 +2,49 @@ var React = require('react');
 var TiffAPI = require('TiffAPI');
 
 var Featured = React.createClass({
-  getInitialState: function () {
-    return {
-      isLoading: false
-    }
-  },
-  getMovieInfo: function () {
-    this.setState({
-      isLoading: true,
-      errorMessage: undefined,
-      title: undefined,
-      caption: undefined,
-      imgUrl: undefined
-    });
-
-    TiffAPI.getFeaturedMovie()
-      .then((info) => {
-        this.setState({
-          isLoading: false,
-          title: info.title,
-          caption: info.caption,
-          imgUrl: info.imgUrl 
-        });
-      }, (err) => {
-        this.setState({
-          isLoading: false,
-          errorMessage: err.message
-        });
-      });
-  },
-  componentDidMount: function () {
-    this.getMovieInfo();
+  propTypes: {
+    isLoadingFeatured: React.PropTypes.bool.isRequired
   },
   render: function () {
     var {
-      isLoading, 
-      title, 
-      caption, 
-      imgUrl, 
-      errorMessage
-    } = this.state;
+      isLoadingFeatured,
+      featuredMovie,
+      errorMessage,
+    } = this.props;
     
-    function renderMovie () {
-      if (isLoading) {
-        return <h3>Loading...</h3>
-      }
-      else {
-        var backgroundStyle = {
-          background: `url(${imgUrl})`,
-          backgroundPosition: '0 40%',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          height: '450px' 
-        };
-        return (
+    if (typeof errorMessage === 'string') {
+      console.log("Error: " + errorMessage);
+      return (
+        <div className="featured">
+          <h3>Oops! Something went wrong, we're working on it</h3>
+        </div>
+      ) 
+    }
+    else if (isLoadingFeatured) {
+      return (
+        <div className="featured">
+          <h3>Loading...</h3>
+        </div>
+      )
+    }
+    else {
+      var backgroundStyle = {
+        background: `url(${featuredMovie.imgUrl})`,
+        backgroundPosition: '0 40%',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: '450px' 
+      };
+      return (
+        <div className="featured">
           <div style={backgroundStyle}> 
-            <h1><span className="text-block">{title}</span></h1>
-            <h3><span className="text-block">{caption}</span></h3>
+            <h1><span className="text-block">{featuredMovie.title}</span></h1>
+            <h3><span className="text-block">{featuredMovie.caption}</span></h3>
             <button className="button">+ MY LIST</button>
           </div>
-        )
-      }
-    }
-    
-    function renderError () {
-      if (typeof errorMessage === 'string') {
-        console.log("Error: " + errorMessage);
-        return <h3>Oops! Something went wrong, we're working on it ;)</h3>
-      }
-    }
-
-    return (
-      <div className="featured">
-        {renderMovie()}
-        {renderError()}
-      </div> 
-    );
+        </div>
+      )  
+    } 
   }
 });
 
--- app/components/TiffApp.jsx
+++ app/components/TiffApp.jsx
@@ -1,14 +1,51 @@
 var React = require('react');
-
 var Nav = require('Nav');
 var Featured = require('Featured');
 var Browse = require('Browse');
+var TiffAPI = require('TiffAPI');
 
 var TiffApp = React.createClass({
+  getInitialState: function () {
+    return {
+      isLoadingFeatured: true,
+      isLoadingBrowse: false,
+      isMovieBriefActive: false,
+      movieBriefId: undefined
+    }
+  },
+  getFeaturedMovie: function () {
+    this.setState({
+      errorMessage: undefined,
+      featuredMovie: undefined
+    });
+
+    TiffAPI.getFeaturedMovie()
+      .then((movie) => {
+        this.setState({
+          isLoadingFeatured: false,
+          featuredMovie: movie
+        });
+      }, (err) => {
+        this.setState({
+          isLoadingFeatured: false,
+          errorMessage: err.message
+        });
+      });
+  },
+  componentDidMount: function () {
+    this.getFeaturedMovie();
+  },
   render: function () {
+    var {
+      isLoadingFeatured,
+      featuredMovie 
+    } = this.state;
+    
     return (
       <div className="tiff-app">
         <Nav/>
+        <Featured featuredMovie={featuredMovie} 
+                  isLoadingFeatured={isLoadingFeatured}/>
         <Browse/>
       </div>
     );
@@ -17,4 +54,3 @@ var TiffApp = React.createClass({
 
 module.exports = TiffApp;
 
-/* <Featured/> */
